575332da8e4722bb8ad43e168b304200cede7251
Nestor Gimenez <ngimenez@spinifexit.com>
2016-10-03 17:19:15 +0800

    new file:   .eslintrc 	modified:   package.json 	new file:   public/js/CommentBox.component.js 	modified:   routes/index.js 	modified:   views/index.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'React Tutorial' });
+    res.render('index', { title: 'React Tutorial' });
+});
+
+router.get('/api/comments', function(req, res, next) {
+    var data = [
+        { id: 1, author: "Pete Hunt", text: "This is one comment" },
+        { id: 2, author: "Jordan Walke", text: "This is *another* comment" }
+    ];
+    res.status(200).json(data);
 });
 
 module.exports = router;
